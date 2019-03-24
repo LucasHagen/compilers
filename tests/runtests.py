e460d833b767864743254536955c95aa13007fe9
Lucas Hagen <lucas_hagen@hotmail.com>
2019-03-24 13:00:57 -0300

    Added test cases, escaped chars and fixed string error: "\"
    Signed-off-by: Lucas Hagen <lucas_hagen@hotmail.com>

--- tests/runtests.py
+++ tests/runtests.py
@@ -1,6 +1,7 @@
 import subprocess
+from pathlib import Path
 from os import listdir
-from os.path import isdir, join
+from os.path import isdir, isfile
 
 def getTestCasesNames(dir):
     files = []
@@ -10,19 +11,19 @@ def getTestCasesNames(dir):
     return files
 
 def readFile(dir, file):
-    f = open(dir + file, "r")
+    f = open(dir + "/" + file, "r")
     content = f.read()
     f.close()
     return content
 
-def getTokens(dir, file):
-    cmd = ['../main.out']
+def getTokens(main_exe, dir, file):
+    cmd = [main_exe]
     input = readFile(dir, file).encode('utf-8')
     result = subprocess.run(cmd, stdout=subprocess.PIPE, input=input)
     return result.stdout.decode('utf-8')
 
-def runTest(case_dir, gold_dir, file):
-    tokens = getTokens(case_dir, file)
+def runTest(main_exe, case_dir, gold_dir, file):
+    tokens = getTokens(main_exe, case_dir, file)
     gold = readFile(gold_dir, file)
     return tokens == gold
 
@@ -30,18 +31,25 @@ def runTest(case_dir, gold_dir, file):
 =============================================
 """
 
-dir = "./test_cases/"
-dir_gold = "./test_golds/"
+script_path = Path(__file__).parent.absolute().as_posix()
+main_exe = script_path + "/../main.out"
+dir = script_path + "/test_cases"
+dir_gold = script_path + "/test_golds"
 cases = getTestCasesNames(dir)
 passed = []
 invalid = []
+
+if (not isfile(main_exe)):
+    print("ERROR! main.out does not exist or is in wrong directory!")
+    print("Try running 'make'")
+    exit()
 
 print("Running tests...")
 print("")
 print("Individual Results:")
 
 for test in cases:
-    result = runTest(dir, dir_gold, test)
+    result = runTest(main_exe, dir, dir_gold, test)
 
     if result:
         passed.append(test)
