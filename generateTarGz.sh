9fe5ab26284f61eb3c6764111c4bdc164942dd52
Lucas Hagen <lucas_hagen@hotmail.com>
2019-04-14 17:29:22 -0300

    Changed Makefile and created script to make .tgz
    Signed-off-by: Lucas Hagen <lucas_hagen@hotmail.com>

---
+++ generateTarGz.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#	Authors:
+#		- Gabriel Pakulski da Silva - 00274701
+#		- Lucas Sonntag Hagen       - 00274698
+
+stages="1 2"
+
+echo
+echo Gerando TGZs...
+echo
+
+for s in $stages; do
+
+    echo "Etapa $s:"
+    cd stage-$s
+    make clean > /dev/null
+    tar czf ../etapa$s.tgz *
+    cd ..
+    echo " - Done!"
+
+done
+
+echo
+echo Done ALL!
